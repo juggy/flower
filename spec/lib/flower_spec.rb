07806238061cca23835448172d705223d9231fb3
Richard Johansson <richard@newsdesk.se>
2011-06-27 22:26:11 +0200

    Specs for Flower#say and #paste

--- spec/lib/flower_spec.rb
+++ spec/lib/flower_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
 describe Flower do
-  describe "#new" do
-    before do
-      @flower = Flower.new
-    end
+  before do
+    @flower = Flower.new
+  end
 
+  describe "#new" do
     %w(messages_url post_url flow_url uuid).each do |attribute|
       it "should set the #{attribute} attributes" do
         @flower.send(attribute).should_not be_nil
@@ -19,6 +19,50 @@ describe Flower do
 
     it "should default users to empty hash" do
       @flower.users.should == {}
+    end
+  end
+
+  describe "#say" do
+    it "should post the message" do
+      def @flower.post(*args)
+        @posted = true if args == ["Hello World", nil]
+      end
+      @flower.say("Hello World")
+      @flower.instance_variable_get("@posted").should be_true
+    end
+
+    it "should mention a user by passing a tag" do
+      def @flower.post(*args)
+        @posted = true if args == ["Hello World", ":highlight:1"]
+      end
+      @flower.say("Hello World", :mention => 1)
+      @flower.instance_variable_get("@posted").should be_true
+    end
+  end
+
+  describe "#paste" do
+    it "should post a message preceded with 4 spaces" do
+      def @flower.post(*args)
+        @posted = true if args.first == "    Hello World"
+      end
+      @flower.paste("Hello World")
+      @flower.instance_variable_get("@posted").should be_true
+    end
+
+    it "should join an array into a multi-line paste" do
+      def @flower.post(*args)
+        @posted = true if args.first == "    Hello\\n    World"
+      end
+      @flower.paste(%w(Hello World))
+      @flower.instance_variable_get("@posted").should be_true
+    end
+
+    it "should mention a user by passing a tag" do
+      def @flower.post(*args)
+        @mentioned = true if args[1] == ":highlight:2"
+      end
+      @flower.paste("Hello World", :mention => 2)
+      @flower.instance_variable_get("@mentioned").should be_true
     end
   end
 end
